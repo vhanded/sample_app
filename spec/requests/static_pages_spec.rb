1c2d9e5fe61b8c20cebfb4df3d8e380b7980ce57
FaCV01 <facv01@Muhammads-MacBook-Pro.local>
2013-10-02 17:30:59 +0800

    Finish layout and route

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,45 +3,45 @@ require 'spec_helper'
 describe "Static pages" do
   
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+  subject {page}
 
   describe "Home page" do
 
+    before {visit root_path}
+
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      should have_content('Sample App')
     end
 
     it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
+      should have_title("#{base_title}")
     end
 
     it "should have the title home" do
-    	visit '/static_pages/home'
-    	expect(page).not_to have_title("Home")
+    	should_not have_title("Home")
     end
   end
 
   describe "Help page" do
   	
   	it "should have the content 'Help'" do
-  		visit "/static_pages/help"
-  		expect(page).to have_title("#{base_title} | Help")
+  		visit help_path
+  		should have_title("#{base_title} | Help")
   	end
   end
 
   describe "About page" do
 
   	it "should have the content 'About Us'" do
-  		visit "/static_pages/about"
-  		expect(page).to have_title("#{base_title} | About Us")
+  		visit about_path
+  		should have_title("#{base_title} | About Us")
   	end
   end
 
   describe "Contact" do
   	it "should have the title 'Contact'" do
-  		visit "/static_pages/contact"
-  		expect(page).to have_title("#{base_title} | Contact")
+  		visit contact_path
+  		should have_title("#{base_title} | Contact")
   	end
   end
 
